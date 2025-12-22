472d1f41cef1aa6f354aaaa91c536bf9e0f7be1f
Dave Anaya <dave@catincloudlabs.com>
2025-12-22 09:25:57 -0600

    update

--- public/assets/js/dashboard-chaos-scatter.js
+++ public/assets/js/dashboard-chaos-scatter.js
@@ -101,7 +101,7 @@
       // Annotations for Quadrants
       annotations: [
         {
-          x: 5, y: 95, 
+          x: 5, y: 93, 
           xref: 'x', yref: 'y',
           text: "<b>The Gamma Casino</b><br>(0-DTE Speculation)",
           showarrow: true,
